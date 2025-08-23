3eccd9ea41fad05b83cfd43fc6307e8a2a3f9193
Sinna_Shan <luth595@gmail.com>
2025-08-23 19:12:26 +0530

    clear display

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
 
+let expression = '';
+let currentInput = '';
 
 // 1. get the values of the buttons
 
+const show = () => {
+    display.textContent = expression || currentInput;
+}
+
 buttons.forEach(button => {
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', () => {
         const value = button.textContent;
-        display.textContent = value;
+        currentInput = value;
+        expression += currentInput;
+        show();
+
+        if (currentInput == "C") {
+            expression = '0'
+            show();
+        }
     });
 })
 // 2. display the values
